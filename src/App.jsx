0718e169125f24b19ae38b3155e424f74159bc68
Wira Kusuma Phandawa <wirakusuma5566@gmail.com>
2025-12-17 13:49:40 +0700

    calc winner function

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ export default function Box(){
   const [btn, setBtn] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true) 
   function handleClick(i) {
+    if(btn[i] || calcWinner(btn)) return;
     const nextBtn = btn.slice();
     // if btn already filled, can't fill it again
     if (btn[i]) return;
@@ -27,10 +28,19 @@ export default function Box(){
     setBtn(nextBtn);
     setXIsNext(!xIsNext)
   }
+
+  const winner = calcWinner(btn)
+  let status = "";
+  if(winner){
+    status = "winner : " + winner
+  } else {
+    status = "Play : " + (xIsNext ? "X" : "O")
+  }
   
   return(
+    <>
+    <div className="status">{status}</div>
     <div className='box'>
-
       <Btn value={btn[0]} onBtnClick={() => handleClick(0)}/>
       <Btn value={btn[1]} onBtnClick={() => handleClick(1)}/>
       <Btn value={btn[2]} onBtnClick={() => handleClick(2)}/>
@@ -41,5 +51,33 @@ export default function Box(){
       <Btn value={btn[7]} onBtnClick={() => handleClick(7)}/>
       <Btn value={btn[8]} onBtnClick={() => handleClick(8)}/>
     </div>
+    </>
   )
+}
+
+function calcWinner(btn) {
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [2,4,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+
+  for (let i = 0; i < lines.length; i++) {
+    // const a = lines[i][0]//0
+    // const b = lines[i][1]//1
+    // const c = lines[i][2]//2
+    // alternative 
+    const [a,b,c] = lines[i];
+    // ["X", "X", "X", "O", "O", null, null, null, null]
+    if(btn[a] && btn[a] === btn[b] && btn[c]) {
+      return btn[a]
+    } 
+  }
+  return false;
 }
